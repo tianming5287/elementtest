28e2204b2dc40b6e60c5f5a636ed2d4fabdca3f0
laijunhua <1640060006@qq.com>
2025-03-02 20:33:42 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+<div class="app">
+  <p>nihao</p>
+  <h1>element-plus</h1>
+</div>
+</template>
+<style>
+/* .app{
+  margin:0;
+  display: block;
+} */
+.app{
+  background-color: aqua;
+  display: block;
+}
+</style>
